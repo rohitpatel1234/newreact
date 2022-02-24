b4343b695fcc89d2206490d918b1fb1b8b631d34
Rohit Patel <190104054@hbtu.ac.in>
2022-02-24 20:37:29 +0530

    my changes

--- src/components/basics/Footerx.js
+++ src/components/basics/Footerx.js
@@ -1,71 +1,80 @@
-import React from 'react'
-import "./Footerx.css"
+import React from "react";
+import "./Footerx.css";
 
 const Footerx = () => {
   return (
-      <>
-<footer className="footer-distributed">
+    <>
+      <footer className="footer-distributed">
+        <div className="footer-left">
+          <h3>
+            Groo<span>Min</span>
+          </h3>
 
-  <div className="footer-left">
+          <p className="footer-links">
+            <a href="#link1" class="link-1">
+              Home
+            </a>
 
-    <h3>Groo<span>Min</span></h3>
+            <a href="#pric">Pricing</a>
 
-    <p className="footer-links">
-      <a href="#link1" class="link-1">Home</a>
+            <a href="#abo">About</a>
 
-      <a href="#pric">Pricing</a>
+            <a href="#con">Contact</a>
+          </p>
 
-      <a href="#abo">About</a>
+          <p className="footer-company-name">Groomin © 2022</p>
+          <p className="footer-company-name">Created By Rohit</p>
+        </div>
 
-      <a href="#con">Contact</a>
-    </p>
+        <div className="footer-center">
+          <div>
+            <i className="fa fa-map-marker"></i>
+            <p>
+              <span>444 S. Cedros Ave</span> Solana Beach, India
+            </p>
+          </div>
 
-                  <p className="footer-company-name">Groomin © 2015</p> 
-                  <p className="footer-company-name">Created By Rohit</p>
-                 
-  </div>
+          <div>
+            <i className="fa fa-phone"></i>
+            <p>+916387162036</p>
+          </div>
 
-  <div className="footer-center">
+          <div>
+            <i className="fa fa-envelope"></i>
+            <p>
+              <a href="mailto:support@company.com">support@company.com</a>
+            </p>
+          </div>
+        </div>
 
-    <div>
-      <i className="fa fa-map-marker"></i>
-      <p><span>444 S. Cedros Ave</span> Solana Beach, India</p>
-    </div>
+        <div className="footer-right">
+          <p className="footer-company-about">
+            <span>About the company</span>
+            Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
+            euismod convallis velit, eu auctor lacus vehicula sit amet.
+          </p>
 
-    <div>
-      <i className="fa fa-phone"></i>
-      <p>+916387162036</p>
-    </div>
+          <div className="footer-icons">
+            <a href="#f">
+              <i class="fa fa-facebook"></i>
+            </a>
+            <a href="#t">
+              <i class="fa fa-twitter"></i>
+            </a>
+            <a href="#l">
+              <i class="fa fa-linkedin"></i>
+            </a>
+            <a href="#j">
+              <i class="fa fa-github"></i>
+            </a>
+            <a href="#j">
+              <i class="fa fa-user"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-    <div>
-      <i className="fa fa-envelope"></i>
-      <p><a href="mailto:support@company.com">support@company.com</a></p>
-    </div>
-
-  </div>
-
-  <div className="footer-right">
-
-    <p className="footer-company-about">
-      <span>About the company</span>
-      Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-    </p>
-
-    <div className="footer-icons">
-
-      <a href="#f"><i class="fa fa-facebook"></i></a>
-      <a href="#t"><i class="fa fa-twitter"></i></a>
-      <a href="#l"><i class="fa fa-linkedin"></i></a>
-      <a href="#j"><i class="fa fa-github"></i></a>
-      <a href="#j"><i class="fa fa-user"></i></a>
-
-    </div>
-
-  </div>
-
-</footer>
-      </>
-  )
-}
-
-export default Footerx
+export default Footerx;
